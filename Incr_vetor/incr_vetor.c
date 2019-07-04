c1b97be0351bbea395a7336f99d9b6b9b563968a
Anderson Carvalho <andersonfelipe01@live.com>
2019-07-04 11:42:56 -0300

    Programa que incrementa e eleva ao quadrado valores de um vetor

---
+++ Incr_vetor/incr_vetor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void incr_vetor (int n, int *v)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        v[i]++;
+}
+
+void quad_vetor(int n, int *v)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        v[i] *= v[i];
+}
+int main(void)
+{
+    int a[] = {1,3,5};
+    incr_vetor(3, a);
+    printf("%d %d %d\n", a[0], a[1], a[2]);
+    quad_vetor(3, a);
+    printf("%d %d %d\n", a[0], a[1], a[2]);
+    return 0;
+}
