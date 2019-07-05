b8d56aaff3b3c92ed754aae50f5474f8401d19de
Anderson Carvalho <andersonfelipe01@live.com>
2019-07-05 09:17:05 -0300

    Resolvido o problema de alocação de memória e mostrar na tela o resultado da matriz.

--- MatTrans/matTrans.c
+++ MatTrans/matTrans.c
@@ -19,43 +19,37 @@ int* transposta (int m, int n, int* mat)
 
 int main(void)
 {
-    /*Declara a i como linha e j como coluna.
+    /*Declara i como linha e j como coluna.
     M e N são o tamanho das linhas e colunas respectivamente */
     int i, j, m=3, n=3;
-    int **mat, **trp;
+    int *mat, *trp;
 
-    /*Aloca a matriz na memória */
-    mat = (int**) malloc(m*sizeof(int*));
-    for (i = 0; i < m; i++)
-        mat[i] = (int*) malloc(n*sizeof(int));
+    /*Aloca a matriz na memória. Matriz por vetor simples.*/
+    mat = (int*) malloc(n*m*sizeof(int));
     
     /*Cria valores para a matriz */
-    for (i = 0; i < m; i++)
-        for (j = 0; j < n; j++)
-            mat[i][j] = (j+2) * (i+1);
+    for (i = 0; i < m*n; i++)
+        mat[i] = (i+2) * (i+1);
     
     /*Mostra a matriz na tela */
-    printf("%d %d %d\n", mat[0][0], mat[0][1], mat[0][2]);
-    printf("%d %d %d\n", mat[1][0], mat[1][1], mat[1][2]);
-    printf("%d %d %d\n\n\n", mat[2][0], mat[2][1], mat[2][2]);
+    printf("%d %d %d\n", mat[0], mat[1], mat[2]);
+    printf("%d %d %d\n", mat[3], mat[4], mat[5]);
+    printf("%d %d %d\n\n", mat[6], mat[7], mat[8]);
 
-    /*Primeiro é preciso alocar o espaço da matriz
-    transposta na memória, em seguida ela recebe
-    o retorno da função */
-    trp = (int**) malloc(m*n*sizeof(int*));
-    *trp = transposta(3,3, *mat);
+    /*A variável da matriz transposta é alocada na
+    memória e em seguida recebe o valor retornado da
+    função. Matriz por vetor simples.*/
+    trp = (int*) malloc(n*m*sizeof(int));
+    trp = transposta(3,3, mat);
     
-    /*Não é possível mostrar a matriz transposta */
-    printf("%d %d %d\n", trp[0][0], trp[0][1], trp[0][2]);
-    //printf("%d %d %d\n", trp[1][0], trp[1][1], trp[1][2]);
-    //printf("%d %d %d\n\n\n", trp[2][0], trp[2][1], trp[2][2]);
+    /*Resolvido problema de alocação de memória.
+    agora é possível mostrar a matriz transposta. */
+    printf("%d %d %d\n", trp[0], trp[1], trp[2]);
+    printf("%d %d %d\n", trp[3], trp[4], trp[5]);
+    printf("%d %d %d\n", trp[6], trp[7], trp[8]);
 
     /*Desaloca toda a memória ocupada pela a matriz */
-    for (i = 0; i < m; i++)
-        free(mat[i]);
     free(mat);
-    for (i = 0; i < m; i++)
-        free(trp[i]);
     free(trp);
 
     return 0;
