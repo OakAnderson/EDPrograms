eae200331378fd00e9b556dcb43171c3912b6d9b
Anderson Carvalho <andersonfelipe01@live.com>
2019-07-05 00:28:08 -0300

    Corrigido o erro da função. Não é possível mostrar a matriz.

--- MatTrans/matTrans.c
+++ MatTrans/matTrans.c
@@ -14,7 +14,6 @@ int* transposta (int m, int n, int* mat)
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
             trp[j*m+i] = mat[i*n+j];
-
     return trp;
 }
 
@@ -38,20 +37,23 @@ int main(void)
     /*Mostra a matriz na tela */
     printf("%d %d %d\n", mat[0][0], mat[0][1], mat[0][2]);
     printf("%d %d %d\n", mat[1][0], mat[1][1], mat[1][2]);
-    printf("%d %d %d\n", mat[2][0], mat[2][1], mat[2][2]);
+    printf("%d %d %d\n\n\n", mat[2][0], mat[2][1], mat[2][2]);
 
-    /*Tentativa de utilizar a função mal formulada. Existe erro */
-    /* *trp = transposta(3,3, *mat);
-
-    printf("%d %d %d\n", trp[0][0], trp[0][1], trp[0][2]);
-    printf("%d %d %d\n", trp[1][0], trp[1][1], trp[1][2]);
-    printf("%d %d %d\n", trp[2][0], trp[2][1], trp[2][2]);
-    */
+    /*Primeiro é preciso alocar o espaço da matriz
+    transposta na memória, em seguida ela recebe
+    o retorno da função */
+    trp = (int**) malloc(m*sizeof(int*));
+    *trp = transposta(3,3, *mat);
+    
+    /*Não é possível mostrar a mastriz transposta */
 
     /*Desaloca toda a memória ocupada pela a matriz */
     for (i = 0; i < m; i++)
         free(mat[i]);
     free(mat);
+    for (i = 0; i < m; i++)
+        free(trp[i]);
+    free(trp);
 
     return 0;
 }
